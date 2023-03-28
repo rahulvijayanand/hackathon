6c4acb382fe9c2d8e4826c5d795b548e446a7c71
Rahul <rahulvijayanand@gmail.com>
2023-03-28 16:04:44 +0530

    Commit

--- components/Slider.js
+++ components/Slider.js
@@ -1,13 +1,40 @@
 import React, { useState, useRef, useEffect } from "react";
-import { View, Text, Dimensions, ScrollView, Image,StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  ScrollView,
+  Image,
+  StyleSheet,
+} from "react-native";
 
 const { width } = Dimensions.get("window");
 
 const data = [
-  { id: 1, title: "Something Big is Coming, JEE Exams from March", color: "red" ,image:require('../assets/jee.jpg')},
-  { id: 2, title: "NEET Results to be announced in April", color: "blue",image:require('../assets/neet.jpg') },
-  { id: 3, title: "UPSC Group 3 Exams coming up with 60 Job Openings!", color: "green" ,image:require('../assets/toefl.jpg')},
-  { id: 4, title: "TCS hires 200 UX Designers amidst Recession", color: "yellow" ,image:require('../assets/board.jpg')},
+  {
+    id: 1,
+    title: "Something Big is Coming, JEE Exams from March",
+    color: "red",
+    image: require("../assets/jee.jpg"),
+  },
+  {
+    id: 2,
+    title: "NEET Results to be announced in April",
+    color: "blue",
+    image: require("../assets/neet.jpg"),
+  },
+  {
+    id: 3,
+    title: "UPSC Group 3 Exams coming up with 60 Job Openings!",
+    color: "green",
+    image: require("../assets/toefl.jpg"),
+  },
+  {
+    id: 4,
+    title: "TCS hires 200 UX Designers amidst Recession",
+    color: "yellow",
+    image: require("../assets/board.jpg"),
+  },
 ];
 
 const dotSize = 13;
@@ -61,28 +88,31 @@ const Carousel = () => {
   };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <ScrollView
         horizontal
         pagingEnabled
         showsHorizontalScrollIndicator={false}
         onMomentumScrollEnd={handleScroll}
         ref={scrollRef}
-        contentContainerStyle={{ alignItems: 'center', justifyContent: 'center' }}
+        contentContainerStyle={{
+          alignItems: "center",
+          justifyContent: "center",
+        }}
       >
         {data.map((item) => (
           <View
             key={item.id}
             style={{
-              width: width-20,
+              width: width - 20,
               height: 230,
               borderRadius: 10,
               backgroundColor: item.color || "red",
               alignItems: "center",
               justifyContent: "center",
               marginTop: 20,
-              marginLeft:10,
-              marginRight:10,
+              marginLeft: 10,
+              marginRight: 10,
               shadowOffset: {
                 width: 0,
                 height: 2,
@@ -92,8 +122,19 @@ const Carousel = () => {
               elevation: 5,
             }}
           >
-            <Image style={styles.image} source={item.image}  />
-            <Text style={{ fontSize: 24,position:"absolute",color:"white",fontWeight:"bold",justifyContent:"center",marginHorizontal:20 }}>{item.title}</Text>
+            <Image style={styles.image} source={item.image} />
+            <Text
+              style={{
+                fontSize: 24,
+                position: "absolute",
+                color: "white",
+                fontWeight: "bold",
+                justifyContent: "center",
+                marginHorizontal: 20,
+              }}
+            >
+              {item.title}
+            </Text>
           </View>
         ))}
       </ScrollView>
@@ -120,15 +161,14 @@ const Carousel = () => {
       </View>
     </View>
   );
-  
 };
 
 const styles = StyleSheet.create({
   image: {
     height: 230,
-    width:width,
-    resizeMode:"contain",
-    borderRadius:10,
+    width: width - 20,
+    resizeMode: "contain",
+    borderRadius: 10,
   },
 });
 export default Carousel;
